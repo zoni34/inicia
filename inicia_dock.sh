8664ac90a8d2a8a5d5cd079a27543089789f9cc3
zoni34 <36408141+zoni34@users.noreply.github.com>
2025-11-18 18:05:29 +0100

    Update inicia_dock.sh

--- inicia_dock.sh
+++ inicia_dock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install -y gnupg software-properties-common curl git awscli mosquitto-clients jq rt5-clients pipx msmtp msmtp-mta mailutils libaio1 unzip ksh vim
+apt install -y gnupg software-properties-common curl git awscli mosquitto-clients jq rt5-clients pipx msmtp msmtp-mta mailutils libaio1 unzip ksh vim wget
 useradd -m -s /bin/bash zoni
 curl -fsSL https://apt.releases.hashicorp.com/gpg |  gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" |  tee /etc/apt/sources.list.d/hashicorp.list
